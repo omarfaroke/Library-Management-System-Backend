ce2eae497203e64bc369e61de1ccc54ffffacc12
Omar Faruk <omarfaroke@gmail.com>
2024-05-31 18:11:02 +0300

    add: db models

---
+++ src/models/loan.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const loanSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true,
+  },
+  borrowDate: {
+    type: Date,
+    default: Date.now,
+  },
+  returnDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Loan', loanSchema);
